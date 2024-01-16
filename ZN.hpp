16177411782b8bbd5e57cee70505daec5ca1b8c7
K40NN <77363726+K40NN@users.noreply.github.com>
2024-01-16 10:58:11 +0000

    Zone Naturel
    les oiseaux

--- ZN.hpp
+++ ZN.hpp
@@ -2,6 +2,29 @@
 
 class ZN : public Parcelle
 {
+  public :
+
+  int surfaceConstructible() const override;
+
+
+};
+
+#include "Constructible.hpp"
+
+class ZAU : public Constructible
+{
+
+  public :
+
+  int surfaceConstructible() const override;
+
+  //surcharge de l'opérateur <<
+  friend std::ostream& operator<<(std::ostream& os, const ZN& zn) {
+      os << "Type : " << zn.proprietaire << endl;
+      os << "proprietaire : " << zau.proprietaire << endl;
+      os << "surface : " << zau.surface << endl;
+      return os;
+  }
 
 
 };
