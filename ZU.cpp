0a5ab39e2ebd88f3f6152edec1ca954c45061919
K40NN <77363726+K40NN@users.noreply.github.com>
2024-01-22 08:38:20 +0000

    partiale release of Zone Urbaine
    conflict with pConstructible

---
+++ ZU.cpp
@@ -0,0 +1,13 @@
+#include "ZU.hpp"
+
+std::ostream& operator<<(std::ostream& os, const ZoneUrbaine& zu){
+  os << "Zone Urbaine : " << std::endl;
+    //os << "Type : " << typeid(*this).name() << endl;
+    //os << "Polygone : " << zu.forme << endl;
+    //os << "Proprietaire : " << zu.proprietaire << std::endl;
+    //os << "Surface : " << zu.surface << std::endl;
+    //os << "% Constructible : " << zu.pConstructible << std::endl;
+    //os << "Surface construite : " << zu.surfaceConstruite << std::endl;
+    //os << "Surface à constuire restante : " << (zu.surface*zu.pConstructible)-zu.surfaceConstruite << endl;
+    return os;
+}
--- ZU.hpp
+++ ZU.hpp
@@ -1,26 +1,18 @@
 #include "ZAU.hpp"
+#include <iostream>
 
-class ZU : public ZAU 
+class ZoneUrbaine : public ZoneAUrbaniser 
 {
 
   private :
 
-  string surfaceConstruite;
+  //string surfaceConstruite;
 
   public :
 
   int surfaceConstructible()const override;
 
   //surcharge de l'opérateur <<
-  friend std::ostream& operator<<(std::ostream& os, const ZU& zu) {
-      os << "Type : " << typeid(*this).name() << endl;
-      os << "Polygone : " << zu.forme << endl;
-      os << "Proprietaire : " << zu.proprietaire << endl;
-      os << "Surface : " << zu.surface << endl;
-      os << "% Constructible : " << zu.pConstructible << endl;
-      os << "Surface construite : " << zu.surfaceConstruite << endl;
-      os << "Surface à constuire restante : " << (zu.surface*zu.pConstructible)-zu.surfaceConstruite << endl;
-      return os;
-  }
-
+  friend std::ostream& operator<<(std::ostream& os, const ZoneUrbaine& zu);
 };
+std::ostream& operator<<(std::ostream& os, const ZoneUrbaine& zu);
