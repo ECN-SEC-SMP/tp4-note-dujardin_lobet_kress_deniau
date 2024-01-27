7957f21da06cf61afe05dcdfd36fe17e28574082
K40NN <77363726+K40NN@users.noreply.github.com>
2024-01-27 12:31:39 +0000

    polygone remake
    polygone coming from kad branch

--- Polygone.hpp
+++ Polygone.hpp
@@ -14,6 +14,7 @@ public:
 
     // Accesseurs
     std::vector<Point2D<T, U>> getSommets() const;
+    float getSurface() const;  // Ajout de la méthode pour obtenir la surface
 
     // Modificateurs
     void setSommets(const std::vector<Point2D<T, U>>& listeSommets);
@@ -21,21 +22,25 @@ public:
 
     // Autres méthodes
     void translate(T deltaX, U deltaY);
+    void calculerSurface();  // Ajout de la méthode pour calculer la surface
 
 private:
     std::vector<Point2D<T, U>> sommets;
+    float surface;  // Ajout de l'attribut surface
 };
 
 // Implémentation des méthodes de la classe Polygone
 
 template <typename T, typename U>
-Polygone<T, U>::Polygone() {}
+Polygone<T, U>::Polygone() : surface(0.0) {}  // Initialisation de la surface à 0
 
 template <typename T, typename U>
-Polygone<T, U>::Polygone(const std::vector<Point2D<T, U>>& listeSommets) : sommets(listeSommets) {}
+Polygone<T, U>::Polygone(const std::vector<Point2D<T, U>>& listeSommets) : sommets(listeSommets) {
+    calculerSurface();  // Calcul de la surface lors de la construction
+}
 
 template <typename T, typename U>
-Polygone<T, U>::Polygone(const Polygone<T, U>& poly) : sommets(poly.sommets) {}
+Polygone<T, U>::Polygone(const Polygone<T, U>& poly) : sommets(poly.sommets), surface(poly.surface) {}
 
 template <typename T, typename U>
 std::vector<Point2D<T, U>> Polygone<T, U>::getSommets() const {
@@ -43,13 +48,20 @@ std::vector<Point2D<T, U>> Polygone<T, U>::getSommets() const {
 }
 
 template <typename T, typename U>
+float Polygone<T, U>::getSurface() const {
+    return surface;
+}
+
+template <typename T, typename U>
 void Polygone<T, U>::setSommets(const std::vector<Point2D<T, U>>& listeSommets) {
     sommets = listeSommets;
+    calculerSurface();  // Mettez à jour la surface lorsque les sommets changent
 }
 
 template <typename T, typename U>
 void Polygone<T, U>::addPoint(const Point2D<T, U>& point) {
     sommets.push_back(point);
+    calculerSurface();  // Mettez à jour la surface lorsque les sommets changent
 }
 
 template <typename T, typename U>
@@ -57,17 +69,23 @@ void Polygone<T, U>::translate(T deltaX, U deltaY) {
     for (auto& point : sommets) {
         point.translate(deltaX, deltaY);
     }
+    calculerSurface();  // Mettez à jour la surface après la translation
 }
 
-template<typename T, typename U>
-std::ostream& operator<<(std::ostream& os, const Polygone<T, U>& poly) {
-  os << "Polygone: ";
+template <typename T, typename U>
+void Polygone<T, U>::calculerSurface() {
+    int n = sommets.size();
+    float surfacePolygone = 0.0;
 
-  for (const auto& point : poly.getSommets()) {
-      os << "(" << point.getX() << ", " << point.getY() << ") ";
-  }
- 
-    return os;
+    for (int i = 0; i < n - 1; ++i) {
+        surfacePolygone += sommets[i].getX() * sommets[i + 1].getY() - sommets[i + 1].getX() * sommets[i].getY();
+    }
+
+    surfacePolygone += sommets[n - 1].getX() * sommets[0].getY() - sommets[0].getX() * sommets[n - 1].getY();
+
+    surfacePolygone = std::abs(surfacePolygone) / 2.0;
+
+    surface = surfacePolygone;
 }
 
 #endif // POLYGONE_HPP
